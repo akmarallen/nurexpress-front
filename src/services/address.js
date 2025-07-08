c4d39ca15d64c6a6f116ee88243088f05eb701c2
Aytkulo <akecan@gmail.com>
2025-07-08 19:45:55 +0600

    Address

--- src/services/address.js
+++ src/services/address.js
@@ -1,5 +1,5 @@
 //Local
 //
-const API_URL = process.env.VUE_APP_API_URL || "http://localhost:8080";
+const API_URL = process.env.VUE_APP_API_URL || "http://90.156.230.28:8080/nur-logistics";
 // Digital Ocean
 export default API_URL;
